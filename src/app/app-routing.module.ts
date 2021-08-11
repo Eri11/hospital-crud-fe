3db174588ddfbd84d8a5c08c3526e64cdf2029ec
Eri11 <erikaeli11@gmail.com>
2021-08-10 21:34:09 -0600

    primer commit
    Hasta aqui TODO funciona!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { MedicosComponent } from './modules/medicos/medicos.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children: [{
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'medicos',
+    component: MedicosComponent,
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
